54cc1d4a9110be8f09fe973010aa32cc81d161a3
vasilievgip <vasilievgip@yandex.ru>
2022-05-14 13:42:44 +0300

    Revert "Удали по просьбе преподавателя Алексея Павлова после проверки домашнего задания Навигация в iOS и жизненный цикл UIViewController"
    This reverts commit 1c4cd69811a66ca3581913ec2a89e9907b8583dc.

---
+++ Netology_IB_Instruments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Андрей Васильев on 06.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
