8be46dadb60fe9ef6e4cebd98d4d327db52e7578
vasilievgip <vasilievgip@yandex.ru>
2022-10-25 20:56:06 +0300

    в процессе

--- Navigation/Navigation/Profile/PhotosViewController.swift
+++ Navigation/Navigation/Profile/PhotosViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class PhotosViewController: UIViewController {
+class PhotosViewController: UIViewController, ImageLibrarySubscriber {
 
     let photos = PhotosArray.makeMockPhotosModel()
 
